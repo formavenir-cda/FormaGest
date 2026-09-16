a3fc2f8c6bb5c77d08541b16365f3db06ee0bd37
Toreh68 <fdurand68@gmail.com>
2026-09-16 12:11:49 +0200

    Interface de réordonnancement des cours

--- frontend/src/app/services/training/course.service.ts
+++ frontend/src/app/services/training/course.service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import type { Observable } from 'rxjs';
+import { of, switchMap } from 'rxjs';
 import type { Course, CourseAssociation } from '../../models/training/course.model';
 import { environment } from '../../../environments/environment.development';
 
@@ -14,7 +15,13 @@ export class CourseService {
   }
 
   create(name: string, associations: CourseAssociation[] = []): Observable<Course> {
-    return this.http.post<Course>(this.apiUrl, { name, associations });
+    return this.http.post<Course>(this.apiUrl, { name }).pipe(
+      switchMap((course) =>
+        associations.length
+          ? this.updateTracks(course.id, this.trackIds(associations))
+          : of(course)
+      )
+    );
   }
 
   update(
@@ -24,11 +31,23 @@ export class CourseService {
   ): Observable<Course> {
     return this.http.put<Course>(
       `${this.apiUrl}/${id}`,
-      { name, associations }
+      { name }
+    ).pipe(
+      switchMap(() =>
+        this.updateTracks(id, this.trackIds(associations))
+      )
     );
   }
 
   delete(id: number): Observable<void> {
     return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+
+  private updateTracks(id: number, trackIds: number[]): Observable<Course> {
+    return this.http.put<Course>(`${this.apiUrl}/${id}/tracks`, trackIds);
+  }
+
+  private trackIds(associations: CourseAssociation[]): number[] {
+    return associations.map((association) => association.trackId);
   }
 }
